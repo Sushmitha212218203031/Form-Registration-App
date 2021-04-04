53e08fc0b88f6875e1decd86f81e0951f9dad32d
Sushmitha212218203031 <78857640+Sushmitha212218203031@users.noreply.github.com>
2021-04-04 16:31:39 +0530

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this,"Sushmitha T 212218203031","you have succesfully filled the form");
+}
